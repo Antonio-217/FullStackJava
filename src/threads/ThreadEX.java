6080f6dde0b21ed4fde13133960d14bb07d35fae
Antonio-217 <antoniocbuenas@gmail.com>
2023-05-15 19:40:01 -0300

    Atualização threads

--- src/threads/ThreadEX.java
+++ src/threads/ThreadEX.java
@@ -1,17 +1,24 @@
 package threads;
 
+import javax.swing.JOptionPane;
+
 public class ThreadEX {
     
     public static void main(String[] args) throws InterruptedException {
         
-        /**/
+        //código da rotina que quero executar em paralelo
         for (int i = 0; i < 10; i++) {
             
-            Thread.sleep(1000);//Da um tempo para o sistema
             //Quero executar esse envio com tempo determinado
             System.out.println("Executando alguma rotina, por exemplo envio de email");
+            Thread.sleep(1000);//Da um tempo para o sistema
         }
-        System.out.println("Chegou ao fim do teste da thread");  
+        //fim do código em paralelo
+        
+        //código do sistema do usuário continua o fluxo de trabalho
+        System.out.println("Chegou ao fim o código do teste da thread"); 
+        //fluxo do sistema, exemplo um cadastro de venda
+        JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário.");
     }
     
 }
